08ee50ea13db8aeaa0e83af43ba5b7168247042f
Vladimir Zverev <zverev@lanbilling.ru>
2017-05-12 20:29:41 +0300

    Добавил normalize.css в зависимости и подключил его в index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import '../node_modules/normalize.css/normalize.css'
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
