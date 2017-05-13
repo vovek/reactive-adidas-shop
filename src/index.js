407195af85485d722ce4a61a95b8631fb287f72c
Vladimir Zverev <zverev@lanbilling.ru>
2017-05-13 16:54:09 +0300

    index.js eslinter formatting

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import '../node_modules/normalize.css/normalize.css'
+import '../node_modules/normalize.css/normalize.css';
 import './assets/css/fonts.css';
 
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
